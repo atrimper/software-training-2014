2f7854ea4ff01dc78333ecb0e78bd67055273e68
Mark Oden <mwoden@gmail.com>
2015-01-07 18:23:39 -0500

    Fixed update to use right repository

--- Update.bat
+++ Update.bat
@@ -1,3 +1,3 @@
 @echo off
-git remote add upstream https://github.com/chopshop-166/frc-2015.git > nul 2>&1
+git remote add upstream https://github.com/chopshop-166/software-training-2014.git > nul 2>&1
 git pull upstream master
